aa9919e127eb87e1e14b69874a34c45593b018d1
Marcos <marcos.martins@liferay.com>
2020-02-20 10:27:53 -0300

    LPS-97802 Implementing form instance record anonymizer

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/anonymizer/DDMFormInstanceRecordUADAnonymizer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/anonymizer/DDMFormInstanceRecordUADAnonymizer.java
@@ -14,9 +14,21 @@
 
 package com.liferay.dynamic.data.mapping.uad.anonymizer;
 
+import com.liferay.dynamic.data.mapping.model.DDMContent;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
+import com.liferay.dynamic.data.mapping.service.DDMContentLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -24,4 +36,72 @@ import org.osgi.service.component.annotations.Component;
 @Component(immediate = true, service = UADAnonymizer.class)
 public class DDMFormInstanceRecordUADAnonymizer
 	extends BaseDDMFormInstanceRecordUADAnonymizer {
+
+	@Override
+	public void autoAnonymize(
+			DDMFormInstanceRecord ddmFormInstanceRecord, long userId,
+			User anonymousUser)
+		throws PortalException {
+
+		super.autoAnonymize(ddmFormInstanceRecord, userId, anonymousUser);
+
+		List<DDMFormInstanceRecordVersion> ddmFormInstanceRecordVersions =
+			ddmFormInstanceRecordVersionLocalService.
+				getFormInstanceRecordVersions(
+					ddmFormInstanceRecord.getFormInstanceRecordId(),
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		_anonymizeDDMFormIntanceRecordVersions(
+			ddmFormInstanceRecordVersions, userId, anonymousUser);
+	}
+
+	@Reference
+	protected DDMContentLocalService ddmContentLocalService;
+
+	@Reference
+	protected DDMFormInstanceRecordVersionLocalService
+		ddmFormInstanceRecordVersionLocalService;
+
+	private void _anonymizeDDMContents(
+		List<DDMContent> ddmContents, long userId, User anonymousUser) {
+
+		ddmContents.forEach(
+			ddmContent -> {
+				if (ddmContent.getUserId() == userId) {
+					ddmContent.setUserId(anonymousUser.getUserId());
+					ddmContent.setUserName(anonymousUser.getFullName());
+				}
+
+				ddmContentLocalService.updateDDMContent(ddmContent);
+			});
+	}
+
+	private void _anonymizeDDMFormIntanceRecordVersions(
+		List<DDMFormInstanceRecordVersion> ddmFormInstanceRecordVersions,
+		long userId, User anonymousUser) {
+
+		List<DDMContent> ddmContents = new ArrayList<>();
+
+		ddmFormInstanceRecordVersions.forEach(
+			ddmFormInstanceRecordVersion -> {
+				if (ddmFormInstanceRecordVersion.getUserId() == userId) {
+					ddmFormInstanceRecordVersion.setUserId(
+						anonymousUser.getUserId());
+
+					ddmFormInstanceRecordVersion.setUserName(
+						anonymousUser.getFullName());
+				}
+
+				ddmContents.add(
+					ddmContentLocalService.fetchDDMContent(
+						ddmFormInstanceRecordVersion.getStorageId()));
+
+				ddmFormInstanceRecordVersionLocalService.
+					updateDDMFormInstanceRecordVersion(
+						ddmFormInstanceRecordVersion);
+			});
+
+		_anonymizeDDMContents(ddmContents, userId, anonymousUser);
+	}
+
 }
