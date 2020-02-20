3f5a1d376c303127415e8e6e7316be16a689ed1a
Marcos <marcos.martins@liferay.com>
2020-02-20 10:33:46 -0300

    LPS-97802 Implementing form instance UAD display

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/display/DDMFormInstanceRecordUADDisplay.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/display/DDMFormInstanceRecordUADDisplay.java
@@ -14,14 +14,104 @@
 
 package com.liferay.dynamic.data.mapping.uad.display;
 
-import com.liferay.user.associated.data.display.UADDisplay;
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.user.associated.data.display.UADDisplay;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, service = UADDisplay.class)
+@Component(immediate = true, service = {UADDisplay.class, DDMFormInstanceRecordUADDisplay.class})
 public class DDMFormInstanceRecordUADDisplay
 	extends BaseDDMFormInstanceRecordUADDisplay {
+
+	@Override
+	public String getEditURL(
+			DDMFormInstanceRecord ddmFormInstanceRecord,
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		String portletNamespace = _portal.getPortletNamespace(
+			DDMPortletKeys.DYNAMIC_DATA_MAPPING_FORM_ADMIN);
+
+		HttpServletRequest httpServletRequest =
+			liferayPortletRequest.getHttpServletRequest();
+
+		Map<String, String[]> params = HashMapBuilder.put(
+			portletNamespace.concat("mvcPath"),
+			new String[] {"/admin/view_form_instance_record.jsp"}
+		).put(
+			portletNamespace.concat("formInstanceRecordId"),
+			new String[] {
+				String.valueOf(ddmFormInstanceRecord.getFormInstanceRecordId())
+			}
+		).put(
+			portletNamespace.concat("formInstanceId"),
+			new String[] {
+				String.valueOf(ddmFormInstanceRecord.getFormInstanceId())
+			}
+		).put(
+			portletNamespace.concat("readOnly"),
+			new String[] {Boolean.FALSE.toString()}
+		).put(
+			portletNamespace.concat("redirect"),
+			new String[] {_portal.getCurrentURL(httpServletRequest)}
+		).build();
+
+		return _portal.getSiteAdminURL(
+			getThemeDisplay(httpServletRequest),
+			DDMPortletKeys.DYNAMIC_DATA_MAPPING_FORM_ADMIN, params);
+	}
+	
+	@Override
+	public String getName(DDMFormInstanceRecord ddmFormInstanceRecord, Locale locale) {
+		return ddmFormInstanceRecord.getVersion();
+	}
+
+	@Override
+	public Class<?> getParentContainerClass() {
+		return DDMFormInstance.class;
+	}
+	
+	@Override
+	public Serializable getParentContainerId(DDMFormInstanceRecord ddmFormInstanceRecord) {
+		return ddmFormInstanceRecord.getFormInstanceId();
+	}
+	
+	@Override
+	public boolean isUserOwned(DDMFormInstanceRecord formInstanceRecord, long userId) {
+		if (formInstanceRecord.getUserId() == userId) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	protected ThemeDisplay getThemeDisplay(
+		HttpServletRequest httpServletRequest) {
+
+		return (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	@Reference
+	private Portal _portal;
+
 }
--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/display/DDMFormInstanceUADDisplay.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/display/DDMFormInstanceUADDisplay.java
@@ -14,13 +14,102 @@
 
 package com.liferay.dynamic.data.mapping.uad.display;
 
-import com.liferay.user.associated.data.display.UADDisplay;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringReader;
+import java.util.Locale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.osgi.service.component.annotations.Component;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.user.associated.data.display.UADDisplay;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, service = UADDisplay.class)
+@Component(immediate = true, service = {UADDisplay.class, DDMFormInstanceUADDisplay.class})
 public class DDMFormInstanceUADDisplay extends BaseDDMFormInstanceUADDisplay {
+	
+	@Override
+	public String getName(DDMFormInstance ddmFormInstance, Locale locale) {
+
+		Document document = _toXMLDocument(ddmFormInstance.getName());
+		
+		return document.getFirstChild().getChildNodes().item(0).getTextContent();
+	}
+	
+	@Override
+	public DDMFormInstance getTopLevelContainer(Class parentContainerClass, Serializable parentContainerId,
+			Object childObject) {
+		
+		if((long)parentContainerId == -1) {
+			return null;
+		}
+		
+		try {
+			
+			if(childObject instanceof DDMFormInstanceRecord) {
+				
+				DDMFormInstanceRecord ddmFormInstanceRecord = (DDMFormInstanceRecord)childObject;
+				
+				return ddmFormInstanceRecord.getFormInstance();
+			}
+			
+			
+		} catch (PortalException e) {
+			_log.error(e, e);
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public Class<?> getParentContainerClass() {
+		return DDMFormInstance.class;
+	}
+	
+	@Override
+	public Serializable getParentContainerId(DDMFormInstance ddmFormInstance) {
+		return 0;
+	}
+	
+	@Override
+	public boolean isUserOwned(DDMFormInstance ddmFormInstance, long userId) {
+		if (ddmFormInstance.getUserId() == userId) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	private Document _toXMLDocument(String xml) {
+		try {
+			
+			DocumentBuilderFactory documentBuilderFactory =  DocumentBuilderFactory.newInstance();
+			
+			DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+			
+			return builder.parse(new InputSource(new StringReader(xml)));
+			
+			
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			_log.error(e, e);
+		}
+		
+		return null;
+	}
+	
+	private static final Log _log = LogFactoryUtil.getLog(
+			DDMFormInstanceUADDisplay.class);
 }
